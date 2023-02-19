c5d59e83b4f569012d44f09c68bc123dff92bbcd
MARCIN <m.palenik2015@gmail.com>
2023-02-19 13:45:49 +0000

    cors problems

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const { getDetails,
         getImage,
         postImage } = require("./controllers/controllers.js");
 
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 
-app.use(cors({origin: '*'}));
+// app.use(cors({origin: '*'}));
 app.use(express.json({limit: '5mb'}));
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get("/api/details", getDetails);
 app.patch("/api/details", patchDetails);
--- seed.js
+++ seed.js
@@ -7,19 +7,19 @@ run = async function(){
     let details = db.collection('details')
 
     let data = await details.set('project_name', {
-        "active_sections": [],
-        "text_image": "test text for this section",
-        "text_buttons": "test text for sections with buttons",
-        "gallery": {
-            "title": "this is title",
-            "img": []
-        },
-        "text": "another test text"
+        "active_sections": ["About", "Gallery"],
+        "text_about": "This is sample of about text.",
+        "image_about": "",
     })
 
+    // let del = await details.delete('project_name')
+
      let getData = await details.get('project_name')
+
+
 
      console.log(getData)
 }
 
-// run()
+
+run()
