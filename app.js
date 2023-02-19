cb2e68aaaddedfb34747e809fda9db21217220b6
MARCIN <m.palenik2015@gmail.com>
2023-02-19 13:49:14 +0000

    post check

--- app.js
+++ app.js
@@ -5,26 +5,16 @@ const { getDetails,
         getImage,
         postImage } = require("./controllers/controllers.js");
 
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
-// app.use(cors({origin: '*'}));
+app.use(cors({origin: '*'}));
 app.use(express.json({limit: '5mb'}));
 
-app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.get("/api/details", getDetails);
-app.patch("/api/details", patchDetails);
+app.post("/api/details", patchDetails);
 
 app.get("/api/image/:url", getImage);
 app.post("/api/image/:url", postImage);
